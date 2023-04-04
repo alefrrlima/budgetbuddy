160de03a9c5b1fcaf031c3e2f2888641dc91fe31
Alexandre F. Lima <108908672+alefrrlima@users.noreply.github.com>
2023-04-04 15:49:20 -0300

    components update

--- src/components/BudgetSelector/BudgetSelector.js
+++ src/components/BudgetSelector/BudgetSelector.js
@@ -1,8 +1,13 @@
 import "./BudgetSelector.css"
 
+import new100 from "../../images/new100-icon.svg"
+
 export default function BudgetSelector(){
    return(
       <div className="budgetSelector">
+         <button>
+            <img src={new100}/>
+         </button>
 
       </div>
    )
