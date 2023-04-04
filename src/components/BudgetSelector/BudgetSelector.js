8e8c121c04b090514ec675cf27a0a5144a0668da
Alexandre F. Lima <108908672+alefrrlima@users.noreply.github.com>
2023-04-04 18:09:11 -0300

    BudgetSelector component created

--- src/components/BudgetSelector/BudgetSelector.js
+++ src/components/BudgetSelector/BudgetSelector.js
@@ -1,14 +1,34 @@
-import "./BudgetSelector.css"
+import './BudgetSelector.css';
 
-import new100 from "../../images/new100-icon.svg"
+import new100 from '../../images/new100-icon.svg';
 
-export default function BudgetSelector(){
-   return(
+export default function BudgetSelector() {
+   return (
       <div className="budgetSelector">
          <button>
-            <img src={new100}/>
+            <svg
+               width="104"
+               height="104"
+               viewBox="0 0 104 104"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg"
+            >
+               <g clip-path="url(#clip0_27_72)">
+                  <rect width="104" height="104" rx="5" fill="white" />
+                  <path
+                     d="M27 52.1337H77M52.0003 77V27"
+                     stroke="#263238"
+                     stroke-width="5"
+                     stroke-linecap="round"
+                  />
+               </g>
+               <defs>
+                  <clipPath id="clip0_27_72">
+                     <rect width="104" height="104" rx="5" fill="white" />
+                  </clipPath>
+               </defs>
+            </svg>
          </button>
-
       </div>
-   )
+   );
 }
