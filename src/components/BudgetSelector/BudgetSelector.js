c3a29ef713540251bb06050eaee7a2139839822d
Alexandre F. Lima <108908672+alefrrlima@users.noreply.github.com>
2023-04-05 10:44:57 -0300

    styles updated

--- src/components/BudgetSelector/BudgetSelector.js
+++ src/components/BudgetSelector/BudgetSelector.js
@@ -1,7 +1,5 @@
 import './BudgetSelector.css';
 
-import new100 from '../../images/new100-icon.svg';
-
 export default function BudgetSelector() {
    return (
       <div className="budgetSelector">
@@ -10,7 +8,6 @@ export default function BudgetSelector() {
                width="104"
                height="104"
                viewBox="0 0 104 104"
-               fill="none"
                xmlns="http://www.w3.org/2000/svg"
             >
                <g clip-path="url(#clip0_27_72)">
@@ -22,11 +19,6 @@ export default function BudgetSelector() {
                      stroke-linecap="round"
                   />
                </g>
-               <defs>
-                  <clipPath id="clip0_27_72">
-                     <rect width="104" height="104" rx="5" fill="white" />
-                  </clipPath>
-               </defs>
             </svg>
          </button>
       </div>
