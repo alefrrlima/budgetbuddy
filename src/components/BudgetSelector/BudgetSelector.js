942feb482d864ee743db557df5bf78cb683252d7
Alexandre F. Lima <108908672+alefrrlima@users.noreply.github.com>
2023-04-14 11:30:35 -0300

    reponsive display updated

--- src/components/BudgetSelector/BudgetSelector.js
+++ src/components/BudgetSelector/BudgetSelector.js
@@ -1,17 +1,32 @@
 import './BudgetSelector.css';
 
 import Budget from '../../components/Budget/Budget.js';
-import Button from "../../components/Button/Button.js"
+import Button from '../../components/Button/Button.js';
 
 export default function BudgetSelector() {
    return (
       <div className="budgetSelector">
-         <div className='displayBudgets'>
-            <Budget title="Orçamento Orçamento" value="R$ 30.000,00" id="#001" />
-            <Budget title="Orçamento Orçamento" value="R$ 1.000,00" id="#002" />
-            <Budget title="Orçamento Orçamento" value="R$ 200,00" id="#003" />
+         <div className="displayBudgets">
+            <Budget
+               title="Orçamento Orçamento"
+               value="R$ 30.000,00"
+               id="#001"
+            />
+            <Budget 
+               title="Orçamento Orçamento" 
+               value="R$ 1.000,00" 
+               id="#002" 
+            />
+            <Budget 
+               title="Orçamento Orçamento" 
+               value="R$ 200,00" 
+               id="#003" 
+            />
          </div>
-         <Button className="blueButton" text='CRIAR ORÇAMENTO'/>
+         <Button 
+            className="blueButton" 
+            text="CRIAR ORÇAMENTO" 
+         />
       </div>
    );
 }
