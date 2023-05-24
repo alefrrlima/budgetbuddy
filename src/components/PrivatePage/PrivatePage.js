5180e4da9d1b3135d558d3d5415fb48bd872c310
Alexandre F. Lima <alexandrelima.beta@gmail.com>
2023-05-24 18:39:58 -0300

    ..

--- src/components/PrivatePage/PrivatePage.js
+++ src/components/PrivatePage/PrivatePage.js
@@ -1,20 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../Button/Button';
+import BBHorizontalLogo from '../BBHorizontalLogo/BBHorizontalLogo';
+
 import '/PrivatePage.css';
 
 export default function PrivatePage() {
+   function toLoginPage(e) {
+      e.preventDefault();
+      useNavigate('/');
+   }
 
-   const loggedUser = localStorage.getItem("loggedUser")
+   const loggedUser = localStorage.getItem('loggedUser');
 
-   if(loggedUser){
-      return(
+   if (loggedUser) {
+      return <div>{/* "Conteúdo da página" */}</div>;
+   } else {
+      return (
          <div>
-            {/* "Conteúdo da página" */}
+            <BBHorizontalLogo />
+            <h1>Ops...</h1>
+            <p>
+               Acredito que você não está logado, siga para a página de login
+               para utilizar a aplicação.
+            </p>
+            <Button onClick={toLoginPage} text="PÁGINA DE LOGIN" className="blueButton"/>
          </div>
-      )
-
+      );
    }
-   else {
-      
-   }
-
-   return <></>;
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,26 +1,21 @@
-import "./HomePage.css"
-import "../../styles/global.css"
+import './HomePage.css';
+import '../../styles/global.css';
 
-import Header from "../../components/Header/Header.js"
-import LightAside from "../../components/LightAside/LightAside.js"
-import BudgetSelector from "../../components/BudgetSelector/BudgetSelector.js"
-import Footer from "../../components/Footer/Footer.js"
+import Header from '../../components/Header/Header.js';
+import LightAside from '../../components/LightAside/LightAside.js';
+import BudgetSelector from '../../components/BudgetSelector/BudgetSelector.js';
+import Footer from '../../components/Footer/Footer.js';
 
-export default function HomePage(){
-
-
-
-
-
-   return(
+export default function HomePage() {
+   return (
       <div className="homePage pageContainer">
-         <Header/>
-         <LightAside 
+         <Header />
+         <LightAside
             mainTitle="Home"
             firstParagraph="Aqui você pode criar e gerenciar seus orçamentos."
          />
-         <BudgetSelector/>
-         <Footer/>
-      </div>   
+         <BudgetSelector />
+         <Footer />
+      </div>
    );
 }
