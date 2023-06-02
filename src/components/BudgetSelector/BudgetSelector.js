31206240325c910b9332acb7fd2a78b4866f3804
Alexandre F. Lima <alexandrelima.beta@gmail.com>
2023-06-02 18:49:00 -0300

    ..

--- src/components/BudgetSelector/BudgetSelector.js
+++ src/components/BudgetSelector/BudgetSelector.js
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import './BudgetSelector.css';
 
@@ -6,36 +7,36 @@ import Budget from '../../components/Budget/Budget.js';
 import Button from '../../components/Button/Button.js';
 
 export default function BudgetSelector() {
-
    const navigate = useNavigate();
+   const user = JSON.parse(localStorage.getItem('loggedUser'));
+   const loggedUser = localStorage.getItem(user.email);
+   const [userData, setUserData] = useState(JSON.parse(loggedUser));
 
-   function toNewBudget(e){
+   function toNewBudget(e) {
       e.preventDefault();
-      navigate("/new-budget")
+      navigate('/new-budget');
    }
 
    return (
       <div className="budgetSelector">
          <div className="displayBudgets">
-            <Budget
-               title="Orçamento Orçamento"
-               value="R$ 30.000,00"
-               id="#001"
-            />
-            <Budget 
-               title="Orçamento Orçamento" 
-               value="R$ 1.000,00" 
-               id="#002" 
-            />
-            <Budget 
-               title="Orçamento Orçamento" 
-               value="R$ 200,00" 
-               id="#003" 
-            />
+            {userData.length == 1 && (
+               <p className="noBudgetMessage">
+                  Nenhum orçamento encontrado, experimente criar um novo.
+               </p>
+            )}
+
+            {userData.length > 1 && (
+               <Budget
+                  title="Orçamento Orçamento"
+                  value="R$ 30.000,00"
+                  id="#001"
+               />
+            )}
          </div>
-         <Button 
-            className="blueButton" 
-            text="CRIAR ORÇAMENTO" 
+         <Button
+            className="blueButton"
+            text="CRIAR ORÇAMENTO"
             onClick={toNewBudget}
          />
       </div>
