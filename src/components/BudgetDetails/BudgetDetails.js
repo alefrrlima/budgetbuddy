fd1df9186804fd3343fead7452f542fefc7d30a5
Alexandre F. Lima <alexandrelima.beta@gmail.com>
2023-06-07 09:19:34 -0300

    ..

--- src/components/BudgetDetails/BudgetDetails.js
+++ src/components/BudgetDetails/BudgetDetails.js
@@ -7,19 +7,6 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function BudgetDetails(props) {
-   const navigate = useNavigate();
-   const params = useParams();
-   const user = JSON.parse(localStorage.getItem('loggedUser'));
-   const loggedUser = localStorage.getItem(user.email);
-   const [userData, setUserData] = useState(JSON.parse(loggedUser));
-   const [userBudgets, setUserBudgets] = useState(userData.slice(1));
-   const budgetID = params.budgetid;
-   const currentBudget = userBudgets.find((budget) => budget.id == budgetID);
-
-   function log(e) {
-      e.preventDefault();
-      console.log(currentBudget);
-   }
 
    return (
       <>
@@ -34,10 +21,8 @@ export default function BudgetDetails(props) {
 
          <div className="buttonsSection">
             <div>
-               <Button onClick={log} text="VOLTAR" className="blueButton" />
-               <Button text="EDITAR" className="blueButton" />
-               <Button text="APAGAR" className="redButton" />
-               <Button text="SALVAR" className="greenButton" />
+               <Button type="button" onClick={props.onClickReturn} text="VOLTAR" className="blueButton" />
+               <Button type="button" onClick={props.onClickDelete} text="APAGAR" className="redButton" />
             </div>
             <AlertMessage />
          </div>
--- src/pages/SelectedBudgetPage/SelectedBudgetPage.js
+++ src/pages/SelectedBudgetPage/SelectedBudgetPage.js
@@ -1,10 +1,31 @@
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+import './SelectedBudgetPage.css';
+
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import LightAside from '../../components/LightAside/LightAside.js';
-import BudgetDetails from '../../components/BudgetDetails/BudgetDetails.js';
 import PrivatePage from '../../components/PrivatePage/PrivatePage';
+import Button from '../../components/Button/Button.js';
+import AlertMessage from '../../components/AlertMessage/AlertMessage.js';
 
 export default function SelectedBudgetPage() {
+   const navigate = useNavigate();
+   const params = useParams();
+   const user = JSON.parse(localStorage.getItem('loggedUser'));
+   const loggedUser = localStorage.getItem(user.email);
+   const [userData, setUserData] = useState(JSON.parse(loggedUser));
+   const [userBudgets, setUserBudgets] = useState(userData.slice(1));
+   const budgetID = params.budgetid;
+   const currentBudget = userBudgets.find((budget) => budget.id == budgetID);
+
+   function log(e) {
+      e.preventDefault();
+      console.log(currentBudget);
+   }
+
    return (
       <PrivatePage>
          <div className="pageContainer selectedBudgetPage">
@@ -13,12 +34,32 @@ export default function SelectedBudgetPage() {
                mainTitle="Orçamento #000"
                firstParagraph="Confira em detalhes, edite se desejar."
             />
-            <BudgetDetails
-               title="Orçamento"
-               addressee="Alexandre Ferreira Lima"
-               category="Serviço"
-               value="R$ 30.000,00"
-            />
+            <div className="budgetDetails">
+               <div>
+                  <span className="budgetTitle">{currentBudget.title}</span>
+                  <span className="budgetInfo">{currentBudget.addressee}</span>
+                  <span className="budgetInfo">{currentBudget.category}</span>
+                  <span className="budgetInfo">{currentBudget.value}</span>
+               </div>
+            </div>
+
+            <div className="buttonsSection">
+               <div>
+                  <Button
+                     type="button"
+                     onClick={log}
+                     text="VOLTAR"
+                     className="blueButton"
+                  />
+                  <Button
+                     type="button"
+                     // onClick={}
+                     text="APAGAR"
+                     className="redButton"
+                  />
+               </div>
+               <AlertMessage />
+            </div>
             <Footer />
          </div>
       </PrivatePage>
