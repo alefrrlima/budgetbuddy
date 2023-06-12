ac330d16f9c7d9a08596c11ab40287e6739a547f
Alexandre F. Lima <alexandrelima.beta@gmail.com>
2023-06-12 01:01:19 -0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-// import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+// import './styles/global.css';
+// import App from './components/App/App.js';
+
+// createRoot(document.querySelector('#root')).render(<App />);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './styles/global.css';
 import App from './components/App/App.js';
 
-createRoot(document.querySelector('#root')).render(<App />);
+ReactDOM.render(<App />, document.querySelector('#root'));
